c93ca8b2259e60dbf95161c4283f5d131d787471
SushaSajesh <92584652+SushaSajesh@users.noreply.github.com>
2021-10-16 15:41:43 +0100

    Add files via upload
    Test Cases

---
+++ WpfApp1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfApp1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("WpfApp1.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0987d78-5df6-42d8-ad97-30dbceba3636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfApp1.Test/WeatherVMTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WpfApp1.Models;
+using WpfApp1.Services;
+using WpfApp1.Services.Data;
+
+namespace WpfApp1.Test
+{
+    [TestClass]
+    public class WeatherVMTest
+    {
+        [TestMethod]
+        public void SearchTextIsMandatoryForWeatherSearch()
+        {
+            var weatherforcastServiceMock = new Mock<IWeatherforcastService>();
+            WeatherVM vm = new WeatherVM(weatherforcastServiceMock.Object);
+            vm.Init();
+
+            vm.SearchText = "";
+            var canSearch = vm.SearchCommand.CanExecute(null);
+            Assert.IsFalse(canSearch);
+
+            vm.SearchText = "London";
+           canSearch = vm.SearchCommand.CanExecute(null);
+            Assert.IsTrue(canSearch);
+        }
+
+        [TestMethod]
+        public void SearchMustSearchWeatherService()
+        {
+            var weatherforcastServiceMock = new Mock<IWeatherforcastService>();
+            weatherforcastServiceMock.Setup(x => x.GetWeatherData(It.IsAny<string>())).ReturnsAsync(new WeatherDataResponse() { Location = new WeatherLocation() {  Name = "London" } });
+            WeatherVM vm = new WeatherVM(weatherforcastServiceMock.Object);
+            vm.Init();
+
+            vm.SearchText = "London";
+            vm.SearchCommand.Execute(null);
+            Assert.IsNotNull(vm.Data);
+            Assert.IsNotNull(vm.Data.Location);
+            Assert.AreEqual(vm.Data.Location.Name, "London");
+        }
+
+        [TestMethod]
+        public void SearchMustSearchWeatherForcastService()
+        {
+            var weatherforcastServiceMock = new Mock<IWeatherforcastService>();
+            weatherforcastServiceMock.Setup(x => x.GetWeatherForcastData(It.IsAny<string>())).ReturnsAsync(new WeatherForcastDataResponse() { Forecast = new Forecast() { ForecastDay = new List<WeatherForecastday>() { new WeatherForecastday() { Hour = new List<WeatherHour>() { new WeatherHour() { Condition = new WeatherCondition() { Text = "Partly Cloudy" } } } } } } });
+            WeatherVM vm = new WeatherVM(weatherforcastServiceMock.Object);
+            vm.Init();
+
+            vm.SearchText = "London";
+            vm.SearchForcastCommand.Execute(null);
+            Assert.IsNotNull(vm.Data);
+            Assert.IsInstanceOfType(vm.Data, typeof(WeatherForcastDataResponse));
+        }
+    }
+}
